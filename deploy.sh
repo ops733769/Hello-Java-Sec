cd9cf05d6716aa70803cc7e9d4ff02804f0994e2
ops733769 <154208323+ops733769@users.noreply.github.com>
2023-12-29 13:02:46 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,9 @@
 #以一种傻瓜交互的形式形式进行原包的Maven打包并可一次成型生成Jar包-完成插桩-封装成完整Docker镜像并自动运行上线。
 stty erase ^H
 
+# 获取当前的年月日时
+current_datetime=$(date '+%Y%m%d%H')
+
 # 交互式输入参数，如果用户不输入则使用默认值
 read -p "请输入IAST代理程序的IP地址（默认为172.20.20.11）: " iast_ip
 iast_ip=${iast_ip:-172.20.20.11}
@@ -10,8 +13,9 @@ iast_url="http://$iast_ip:81/api/agent/api/iast/download?version=MQ=="
 read -p "请输入Maven项目所在目录（默认为/root/Hello-Java-Sec）: " project_directory
 project_directory=${project_directory:-/root/Hello-Java-Sec}
 
-read -p "请输入Maven项目构建完成后的镜像名称（默认为hellojavasec:v0.1）: " image_name
-image_name=${image_name:-hellojavasec:v0.1}
+# 交互式输入版本字段，如果用户不输入则使用默认的年月日时格式
+read -p "请输入Maven项目构建完成后的镜像名称（默认为hellojavasec:v$current_datetime）: " image_name
+image_name=${image_name:-hellojavasec:v$current_datetime}
 
 read -p "请输入启用的容器名称（默认为hellojavasec_iast）: " container_name
 container_name=${container_name:-hellojavasec_iast}
@@ -30,8 +34,7 @@ docker run -it --rm \
     --name MavenProject \
     -v "$project_directory":/root/Hello-Java-Sec \
     -w /root/Hello-Java-Sec \
-    adoptopenjdk/maven-openjdk11:latest \
-    mvn dependency:go-offline  \
+    adoptopenjdk/maven-openjdk8:latest \
     mvn clean package -U -DskipTests \
     && docker build -t "$image_name" . \
-    && docker run -itd --name "$final_container_name" -p "$external_port":8888  "$image_name"
+    && docker run -itd --name "$final_container_name" -p "$external_port":8888   "$image_name"
